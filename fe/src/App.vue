0dea0a9bbb84ebf9d02aeb502baf4f9420da18a9
radek-zitek-cloud <radek@zitek.cloud>
2025-06-13 09:27:04 +0000

    feat: initialize Vue 3 project with Vuetify, Pinia, and Vue Router
    - Added logo.svg asset for branding.
    - Configured auto-imports for Vue and Pinia in auto-imports.d.ts.
    - Registered global components in components.d.ts.
    - Created AppFooter and HelloWorld components with Vuetify styling.
    - Set up default layout with AppFooter.
    - Bootstrapped application in main.ts and registered plugins.
    - Implemented automatic routing for pages in router/index.ts.
    - Established Pinia store for application state management.
    - Configured Vite with necessary plugins for Vue, Vuetify, and auto-imports.
    - Added SASS settings for Vuetify customization.
    - Documented components, layouts, pages, plugins, and stores for clarity.

---
+++ fe/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-responsive class="border rounded">
+    <v-app>
+      <v-app-bar :elevation="2">
+        <template v-slot:prepend>
+          <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        </template>
+        <v-app-bar-title>Fulcrum</v-app-bar-title>
+        <template v-slot:append>
+          <v-btn icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'" @click="onClick" slim></v-btn>
+          <v-btn icon="mdi-dots-vertical"></v-btn>
+        </template>
+      </v-app-bar>
+
+      <v-navigation-drawer v-model="drawer" :locations="left">
+        <v-list>
+          <v-list-item title="Navigation drawer"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main>
+        <v-container>
+          <router-view />
+        </v-container>
+      </v-main>
+    </v-app>
+  </v-responsive>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const theme = ref('light')
+
+function onClick() {
+  theme.value = theme.value === 'light' ? 'dark' : 'light'
+}
+</script>
