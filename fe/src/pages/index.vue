952cd4eaa68590453c7d07ac16853818c46a8b4d
Radek Zitek <radek.zitek@proton.me>
2025-06-14 12:44:50 +0200

    feat: update homepage layout and content, remove unused components

--- fe/src/pages/index.vue
+++ fe/src/pages/index.vue
@@ -1,5 +1,22 @@
 <template>
-  <HomeCard />
+  <v-container fill-height>
+    <v-row align="center" justify="center" class="fill-height">
+      <v-col cols="12" sm="8" md="6" lg="4">
+        <v-card max-width=400 :elevation="24">
+          <v-img src="@/assets/fulcrum-mid.png" cover></v-img>
+          <v-card-title class="text-h5">Fulcrum</v-card-title>
+          <v-card-subtitle class="
+          ">A Full-Stack Web Application for Departmental Management</v-card-subtitle>
+          <v-card-text>
+            Fulcrum is a full-stack web application designed to serve as the central coordination hub for departmental
+            management. It provides an integrated environment for handling human resources, performance tracking, budget
+            oversight, and workload planning—turning disjointed spreadsheets and tools into a single, consistent source
+            of operational truth.
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts" setup>
